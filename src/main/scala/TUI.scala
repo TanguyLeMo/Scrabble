1788c3caddd84951165f6111d077a6032e840421
Tanguy Le Morzadec <rataelios@gmail.com>
2024-04-05 10:27:41 +0200

    making code denser, more functional and clearer.

--- src/main/scala/TUI.scala
+++ src/main/scala/TUI.scala
@@ -1,17 +1,20 @@
 import scala.language.postfixOps
 
 class ScrabbleField(row: Int, cols: Int) {
+  val columns: Int = cols
+  val rows: Int = row
+  val field: Array[Array[Char]] = Array.ofDim[Char](rows, cols)
+  val emptyTile = '_'
+  val numofAlphabet = 26
   val numSymolPerColumn: Int = 2 + Math.ceil(rows.toDouble / numofAlphabet.toDouble).toInt
-   val columns = cols
-   val rows = row
-   val field = Array.ofDim[Char](rows, cols)
-   val emptyTile = '_'
-   val numofAlphabet = 26;
-  def placeTile(row: Int, col: Int, tile: Char): Unit = {
+
+  def placeTile(row: Int, col: Int, tile: Char): Unit =
     if (0 <= col && col < cols && 0 <= row && row < rows)
       field(row)(col) = tile
-  }
-  override def toString: String =s"    ${labelingXAxis(1)}\n${goThroughRow(0)}"
+
+  override def toString: String =
+    s"    ${labelingXAxis(1)}\n${goThroughRow(0)}"
+
   def labelingXAxis(currcolum: Int): String =
     if (currcolum > columns) "" else getLetter(currcolum) + addSpace(numSymolPerColumn - getLetter(currcolum).length) + labelingXAxis(currcolum + 1)
 
@@ -19,13 +22,14 @@ class ScrabbleField(row: Int, cols: Int) {
     case n if n <= 0 => " "
     case n => " " + addSpace(n - 1)
   }
+
   def getLetter(n: Int): String =
     if (n <= 0) "" else getLetter((n - 1) / 26) + ('A' + (n - 1) % 26).toChar
 
   def getMaxNumOfChar: Int = Math.ceil(columns / numofAlphabet).asInstanceOf[Int]
+
   def goThroughRow(currentRow: Int): String =
     if (currentRow >= rows) "" else s"${currentRow.toString.padTo(3, ' ')} ${gothroughColumn(currentRow, 0) + "\n"}${goThroughRow(currentRow + 1)}"
-
 
   def gothroughColumn(currentRow: Int, currentColumn: Int): String = {
     val nextCol = currentColumn + 1
@@ -48,8 +52,7 @@ class ScrabbleField(row: Int, cols: Int) {
 }
 
 object Main extends App {
-
-  val numbsForBothSide = 5
+  val numbsForBothSide = 51
   val field = new ScrabbleField(numbsForBothSide, numbsForBothSide)
 
   field.placeTile(3, 3, 'A')

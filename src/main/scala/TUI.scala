08421824250e15af63f3f0fd2dfb27a76b560d37
Tanguy Le Morzadec <rataelios@gmail.com>
2024-04-05 12:24:34 +0200

    Merge branch 'scalalikeTUI' into DEV

--- src/main/scala/TUI.scala
+++ src/main/scala/TUI.scala
@@ -55,6 +55,7 @@ object Main extends App {
   val numbsForBothSide = 51
   val field = new ScrabbleField(numbsForBothSide, numbsForBothSide)
 
+  print("there")
   field.placeTile(3, 3, 'A')
   field.placeTile(7, 7, 'B')
   field.placeTile(9, 5, 'C')

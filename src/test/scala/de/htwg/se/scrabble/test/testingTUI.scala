45d5a3d0acae5a837fd986a0a81f67bffe157471
Tanguy Le Morzadec <rataelios@gmail.com>
2024-04-17 17:08:25 +0200

    first written tests for constructor, size of Scrabblefield and placeTile

--- src/test/scala/de/htwg/se/scrabble/test/testingTUI.scala
+++ src/test/scala/de/htwg/se/scrabble/test/testingTUI.scala
@@ -4,8 +4,24 @@ import org.scalatest.matchers.should.Matchers._
 
 
 class testingTUI extends AnyWordSpec {
-
+val standardScrabbleFieldSize = 15
 // place Tile Tanguy
+"A Scrabble field is a datatype that contains a two-dimensional arrays of character. A Matrix" when {
+  "empty" should {
+    "be created by using a dimention and a sample cell" in {
+      val scrabbleField = new ScrabbleField(standardScrabbleFieldSize, standardScrabbleFieldSize)
+      scrabbleField.field.length should be (standardScrabbleFieldSize)
+    }
+ /*   "The function \"PlaceTile\" should be placing a character at given position with given character. A tile" when {
+      "placed" should {
+        val scrabbleField = new ScrabbleField(standardScrabbleFieldSize, standardScrabbleFieldSize)
+        val character = 'B'
+        scrabbleField.placeTile(3, 7, character) shouldEqual(character)
+
+      } */
+    }
+  }
+}
 
 // addSpaces Tanguy
 

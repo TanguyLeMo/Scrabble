894d1088a94f67e2870cbadcaa589eab99190554
HBummele <hannes.bummele@yahoo.de>
2024-04-25 22:12:05 +0200

    created a TUI for the Playingfield allowing us to place words on the field. Added a Funktion to translate a Letter-Number to Number-Number coordinate

--- src/main/scala/de/htwg/se/scrabble/test/TUI.scala
+++ src/main/scala/de/htwg/se/scrabble/test/TUI.scala
@@ -1,53 +1,52 @@
-package de.htwg.se.scrabble.test
+package de.htwg.se.scrabble
+import de.htwg.se.scrabble.ScrabbleField
+import scala.io.StdIn.readLine
 
-import scala.language.postfixOps
-
-class ScrabbleField(rows: Int, columns: Int) {
-  val field: Array[Array[Char]] = Array.ofDim[Char](rows, columns)
-  val emptyTile = '_'
-  val numofAlphabet = 26
-  val numSymolPerColumn: Int = 2 + Math.ceil(rows.toDouble / numofAlphabet.toDouble).toInt
-
-  def placeTile(row: Int, col: Int, tile: Char): Unit =
-    if (0 <= col && col < columns && 0 <= row && row < rows)
-      field(row)(col) = tile
-
-  override def toString: String =
-    s"    ${labelingXAxis(1)}\n${concatenateCurrentRow(0)}"
-
-  def labelingXAxis(currcolum: Int): String =
-    if (currcolum > columns) "" else getLetter(currcolum) + addSpace(numSymolPerColumn - getLetter(currcolum).length) + labelingXAxis(currcolum + 1)
-
-  def addSpace(numSpaceToAdd: Int): String = numSpaceToAdd match {
-    case n if n <= 0 => " "
-    case n => " " + addSpace(n - 1)
-  }
-
-  def getLetter(n: Int): String =
-    if (n <= 0) "" else getLetter((n - 1) / 26) + ('A' + (n - 1) % 26).toChar
-
-  def concatenateCurrentRow(currentRow: Int): String =
-    if (currentRow >= rows) "" else s"${currentRow.toString.padTo(3, ' ')} ${concatenateColumnsOfCurrentRow(currentRow, 0) + "\n"}${concatenateCurrentRow(currentRow + 1)}"
-
-  def concatenateColumnsOfCurrentRow(currentRow: Int, currentColumn: Int): String = {
-    val nextCol = currentColumn + 1
-    val midRow = (rows - 1) / 2
-    val midCol = (columns - 1) / 2
-
-    if (currentColumn >= columns) ""
-    else if (field(currentRow)(currentColumn) == 0) {
-      emptyTile + addSpace(numSymolPerColumn - 1) + concatenateColumnsOfCurrentRow(currentRow, nextCol)
-    } else field(currentRow)(currentColumn) + addSpace(numSymolPerColumn - 1) + concatenateColumnsOfCurrentRow(currentRow, nextCol)
-  }
+@main def run: Unit = {
+  val field = new ScrabbleField(Vector.fill(15)(Vector.fill(15)('_')))
+  println(field)
+  getInputAndPrintLoop(field)
 }
 
-/*
-  object Main extends App {
-  val numbsForBothSide = 42
-  val field = new ScrabbleField(numbsForBothSide, numbsForBothSide)
+def getInputAndPrintLoop(field: ScrabbleField): Unit = {
+  println("Enter your Word, Coordinate and Direction(H|V) example: myWord A 0 H")
+  val input = readLine()
+  parseInput(input, field) match
+    case None => field
+    case Some(newfield) =>
+      println(newfield)
+      getInputAndPrintLoop(newfield)
+}
 
-  field.placeTile(3, 3, 'A')
-  field.placeTile(7, 7, 'B')
-  field.placeTile(9, 5, 'C')
-  println(field.numSymolPerColumn) 
-}*/
+def parseInput(input: String, field: ScrabbleField): Option[ScrabbleField] = {
+  input match
+    case "exit" => None
+    case _ =>
+      val inputArray = input.split(" ")
+      if (inputArray.length != 4){
+        println("invalid input")
+        Some(field)
+      }else if(!("""[A-Z,a-z]+""".r matches inputArray(1)) || !("""[0-9]+""".r matches inputArray(2))){
+        println("invalid coordinates")
+        Some(field)
+      } else {
+        val direction = inputArray(3) match
+          case "H" => "H"
+          case "V" => "V"
+          case _ => inputArray(3) + " is not a correct direction"
+
+        val word = inputArray(0)
+        val coordinates = field.translateCoordinate(inputArray(1) + " " + inputArray(2))
+        val xCoordinate = coordinates._1
+        val yCoordinate = coordinates._2
+        if (!(direction == "H" | direction == "V")) {
+          println(direction)
+          Some(field)
+        } else if (!field.wordFits(xCoordinate, yCoordinate, direction.charAt(0), word)) {
+          println("Word doesnt fit")
+          Some(field)
+        } else {
+          Some(field.placeWord(xCoordinate, yCoordinate, direction.charAt(0), word))
+        }
+      }
+}

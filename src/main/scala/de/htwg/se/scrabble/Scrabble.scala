ee6bd11b3ba284ee3a36fc4f4d68dd9f4aa478de
TanguyLeMo <tanguylm02@gmail.com>
2024-05-03 22:12:41 +0200

    test to check if coverall is working properly

--- src/main/scala/de/htwg/se/scrabble/Scrabble.scala
+++ src/main/scala/de/htwg/se/scrabble/Scrabble.scala
@@ -4,7 +4,7 @@ import scala.io.StdIn.readLine
 import aview.TUI
 import controller.Controller
 import model.ScrabbleField
-
+//This is to test the coverall feature
 @main def run(): Unit = {
     val field = new ScrabbleField(15)
     val controller = Controller(field)
@@ -12,10 +12,9 @@ import model.ScrabbleField
     println("Welcome to Scrabble")
     println(controller.toString)
     println("Enter your Word, Coordinate and Direction(H|V) example: myWord A 0 H")
-
     while(!tui.processInputLine(readLine()).equals("exit")) {
     }
     println("Goodbye")
 }
 
-  
+

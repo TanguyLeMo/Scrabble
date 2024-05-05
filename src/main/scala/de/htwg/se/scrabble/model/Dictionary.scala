107e14aea19f4888f9be66e471adc96064f6a28a
TanguyLeMo <tanguylm02@gmail.com>
2024-05-05 17:23:11 +0200

    cleaned up the class

--- src/main/scala/de/htwg/se/scrabble/model/Dictionary.scala
+++ src/main/scala/de/htwg/se/scrabble/model/Dictionary.scala
@@ -1,14 +1,8 @@
 package de.htwg.se.scrabble.model
-
 import scala.collection.immutable.HashSet
-
 class Dictionary(val set: HashSet[String]) {
   val file: String = "englishWordList.txt"
-
-  // Auxiliary constructor
   def this() = this(HashSet.empty[String])
-
-  // Read lines from file and create a new Dictionary
   def readLines: Dictionary = {
     val source = scala.io.Source.fromResource("englishWordList.txt")
     val lines = try source.getLines.toList finally source.close()
@@ -18,7 +12,4 @@ class Dictionary(val set: HashSet[String]) {
   def contains(word: String): Boolean = set.contains(word)
   def addWord(word: String): Dictionary = new Dictionary(set + word)
   def removeWord(word: String): Dictionary = new Dictionary(set - word)
-
-  // Auxiliary method to call readLines if no parameters are given
-
 }

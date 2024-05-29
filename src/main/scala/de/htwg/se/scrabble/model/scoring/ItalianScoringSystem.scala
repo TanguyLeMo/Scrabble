8f061bc0e02cd8062734f1c2bd95c1789b1a1f39
TanguyLeMo <tanguylm02@gmail.com>
2024-05-29 15:51:11 +0200

    updated reamde

--- src/main/scala/de/htwg/se/scrabble/model/scoring/ItalianScoringSystem.scala
+++ src/main/scala/de/htwg/se/scrabble/model/scoring/ItalianScoringSystem.scala
@@ -2,6 +2,7 @@ package de.htwg.se.scrabble.model.scoring
 import de.htwg.se.scrabble.model.Stone
 
 class ItalianScoringSystem extends ScoringSystem{
+  //TODO: Implement creation of ItalianScoringSystem
 override def determinPoints(stone: Stone): Int = stone.symbol match {
   case 'O' | 'A' | 'I' | 'E' => 1
   case 'C' | 'R' | 'S' | 'T' => 2

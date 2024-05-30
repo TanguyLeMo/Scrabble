afc6cc7148e510e22e09b19d0c3e63d9e1790b06
TanguyLeMo <tanguylm02@gmail.com>
2024-05-30 19:33:35 +0200

    added undo redo

--- src/test/scala/de/htwg/se/scrabble/model/ScoringSystem/ScoringSystemSpec.scala
+++ src/test/scala/de/htwg/se/scrabble/model/ScoringSystem/ScoringSystemSpec.scala
@@ -9,18 +9,16 @@ import de.htwg.se.scrabble.aview.languages.LanguageEnum.{ENGLISH, FRENCH, GERMAN
 import de.htwg.se.scrabble.model.scoring.*
 
 class ScoringSystemSpec extends AnyWordSpec with Matchers {
+  val scrabbleField = new ScrabbleField(15, ENGLISH)
   "A ScoringSystem" when {
     "collecting points" should {
       "calculate the correct score for a word" in {
-        val scrabbleField = new ScrabbleField(new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(), new Dictionary().readLines(ENGLISH), new StandardSquareFactory, ENGLISH)
 
         val horn = scrabbleField.placeWord(7, 5, 'V', "HORN")
         val score = horn.scoringSystem.collectPoints(horn.matrix, 5, 7, 'H', "HORN")
         score shouldBe 7
       }
       " calculate the correct score for a word with a double word square" in {
-        val scrabbleField = new ScrabbleField(new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(), new Dictionary().readLines(ENGLISH), new StandardSquareFactory, ENGLISH)
-
         val horn = scrabbleField.placeWord(0, 0, 'V', "HORN")
         print(horn)
         val score = horn.scoringSystem.collectPoints(horn.matrix, 0, 0, 'H', "horn")
@@ -29,8 +27,6 @@ class ScoringSystemSpec extends AnyWordSpec with Matchers {
 
       " When placed HORN on the board" should {
         "calculate the correct score" in {
-          val scrabbleField = new ScrabbleField(new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(), new Dictionary().readLines(ENGLISH), new StandardSquareFactory, ENGLISH)
-
           val horn = scrabbleField.placeWord(0, 0, 'H', "DBKJQ")
           val score = horn.scoringSystem.collectPoints(horn.matrix, 0, 0, 'V', "DBKJQ_")
           score shouldBe 108
@@ -38,45 +34,31 @@ class ScoringSystemSpec extends AnyWordSpec with Matchers {
       }
       "When using a french scoring system" should {
         "calculate the correct score" in {
-          val scrabbleField = new ScrabbleField(new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(), new Dictionary().readLines(FRENCH), new StandardSquareFactory, FRENCH)
-
           val horn = scrabbleField.placeWord(0, 0, 'H', "ADBFJK")
           val score = horn.scoringSystem.collectPoints(horn.matrix, 0, 0, 'V', "ADBFJK")
           score shouldBe 96
         }
       }
+      
       "When using a german scoring system" should {
         "calculate the correct score" in {
-          val scrabbleField = new ScrabbleField(new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(), new Dictionary().readLines(GERMAN), new StandardSquareFactory, GERMAN)
 
           val horn = scrabbleField.placeWord(0, 0, 'H', "EHMCÄÖY")
           val score = horn.scoringSystem.collectPoints(horn.matrix, 0, 0, 'V', "EHMCÄÖY")
           score shouldBe 114
         }
         "return 0 when the word is empty" in {
-          val scrabbleField = new ScrabbleField(new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(), new Dictionary().readLines(GERMAN), new StandardSquareFactory, GERMAN)
           val horn = scrabbleField.placeWord(0, 0, 'H', " ")
           val score = horn.scoringSystem.collectPoints(horn.matrix, 0, 0, 'V', " ")
           score shouldBe 0
           }
         "return 0 for French" in {
-          val scrabbleField = new ScrabbleField(
-            new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(),
-            new Dictionary().readLines(LanguageEnum.FRENCH),
-            new StandardSquareFactory,
-            LanguageEnum.FRENCH
-          )
+          
           val horn = scrabbleField.placeWord(0, 0, 'H', " ")
           val score = horn.scoringSystem.collectPoints(horn.matrix, 0, 0, 'V', " ")
           score shouldBe 0
         }
         "return 0 for English" in {
-          val scrabbleField = new ScrabbleField(
-            new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(),
-            new Dictionary().readLines(LanguageEnum.ENGLISH),
-            new StandardSquareFactory,
-            LanguageEnum.ENGLISH
-          )
           val horn = scrabbleField.placeWord(0, 0, 'H', " ")
           val score = horn.scoringSystem.collectPoints(horn.matrix, 0, 0, 'V', " ")
           score shouldBe 0
@@ -85,13 +67,11 @@ class ScoringSystemSpec extends AnyWordSpec with Matchers {
       }
       "When using an Italian scoring system" should {
         "calculate the correct score" in {
-          val scrabbleField = new ScrabbleField(new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(), new Dictionary().readLines(LanguageEnum.ITALIAN), new StandardSquareFactory, LanguageEnum.ITALIAN)
           val horn = scrabbleField.placeWord(0, 0, 'H', "OCLBGQ.")
           val score = horn.scoringSystem.collectPoints(horn.matrix, 0, 0, 'V', "OCLBGQ.")
           score shouldBe 102
         }
         "When there is no stone placed on a square the isEmpty method should return true" in {
-          val scrabbleField = new ScrabbleField(new Matrix(Vector.fill(15, 15)(new StandardSquareFactory().createDoubleSquare(new Stone()))).init(), new Dictionary().readLines(LanguageEnum.ITALIAN), new StandardSquareFactory, LanguageEnum.ITALIAN)
           for(i <- 0 to 14)
             for(j <- 0 to 14)
               scrabbleField.matrix.field(i)(j).isEmpty shouldBe true
